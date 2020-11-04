29c3c5f47f8a329fa8b04457cb72c3b091396859
Dragan Begonja <dragan.begonja@ecx.io>
2020-11-04 16:58:25 +0100

    Game lost

--- src/main/java/com/example/aep201111/Game.java
+++ src/main/java/com/example/aep201111/Game.java
@@ -2,8 +2,29 @@ package com.example.aep201111;
 
 public class Game
 {
+    private boolean lost = false;
+
+    public Game()
+    {
+
+    }
+
+    public Game(String test)
+    {
+    }
+
     public boolean isRunning()
     {
-        return true;
+        return !lost;
+    }
+
+    public void guess(String t)
+    {
+        this.lost = true;
+    }
+
+    public boolean isLost()
+    {
+        return lost;
     }
 }
--- src/test/java/com/example/aep201111/HangmanTest.java
+++ src/test/java/com/example/aep201111/HangmanTest.java
@@ -1,15 +1,37 @@
 package com.example.aep201111;
 
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-class HangmanTest {
+class HangmanTest
+{
 
-	@Test
-	void isGameRunningAtStart()
-	{
-		Game game = new Game();
-		assertTrue(game.isRunning());
-	}
+    @Test
+    void isGameRunningAtStart()
+    {
+        Game game = new Game();
+        assertTrue(game.isRunning());
+    }
 
+    @Test
+    void isGameLostAfterElevenWrongGuesses()
+    {
+        Game game = new Game("test");
+        game.guess("x");
+        game.guess("x");
+        game.guess("x");
+        game.guess("x");
+        game.guess("x");
+        game.guess("x");
+        game.guess("x");
+        game.guess("x");
+        game.guess("x");
+        game.guess("x");
+        game.guess("x");
+
+        assertTrue(game.isLost());
+        assertFalse(game.isRunning());
+    }
 }
