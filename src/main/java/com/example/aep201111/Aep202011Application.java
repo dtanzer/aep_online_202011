02e5a17cfb2b41fdad5afa5f8bba4ac8dd41c837
David Tanzer <david@davidtanzer.net>
2020-11-04 07:34:05 +0100

    Added empty spring boot application

---
+++ src/main/java/com/example/aep201111/Aep202011Application.java
@@ -0,0 +1,13 @@
+package com.example.aep201111;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aep202011Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aep202011Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/aep201111/Aep202011ApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.aep201111;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.fail;
+
+class HangmanTest {
+
+	@Test
+	void fixmeFindABetterName() {
+		fail("FIXME: Find a better name!");
+	}
+
+}
