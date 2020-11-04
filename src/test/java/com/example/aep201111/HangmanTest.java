cf785e546e913c3965c86cc7c8fd7f0644b471af
Dragan Begonja <dragan.begonja@ecx.io>
2020-11-04 14:01:35 +0100

    Add first couple of tests and junit 5

--- src/test/java/com/example/aep201111/HangmanTest.java
+++ src/test/java/com/example/aep201111/HangmanTest.java
@@ -1,15 +1,41 @@
 package com.example.aep201111;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.fail;
 
 class HangmanTest {
 
+	@InjectMocks
+	private Hangman hangman;
+
+
 	@Test
-	void fixmeFindABetterName() {
-		fail("FIXME: Find a better name!");
+	void test_startGame()
+	{
+		fail("Do we need this one");
 	}
 
+	@Test
+	void test_pickGameWord()
+	{
+		final String result = hangman.pickGameWord();
+
+		assertThat(result).isInstanceOf(String.class);
+	}
+
+	@Test
+	void test_printGameWord()
+	{
+		fail("Implement the print word method");
+	}
+
+	@Test
+	void test_attempt()
+	{
+		fail("Implement attempt");
+	}
 }
