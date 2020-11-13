4247720c9c426fe62254d8976777ac6b90ac8d8d
j.wolfsteiner <juergen.wolfsteiner@ecx.io>
2020-11-13 12:46:54 +0100

    sdf

--- src/main/java/com/example/aep201111/Aep202011Application.java
+++ src/main/java/com/example/aep201111/Aep202011Application.java
@@ -8,6 +8,14 @@ public class Aep202011Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Aep202011Application.class, args);
+
+		System.out.println("seas");
+
+		System.out.println("BasePrice:");
+		String baseprice = System.console().readLine();
+
+		System.out.println("Total Price: 1200");
+
 	}
 
 }
