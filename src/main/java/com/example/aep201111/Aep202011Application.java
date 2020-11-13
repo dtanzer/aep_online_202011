d0ff64008f6be6dd0c035a2d88828c2a1b29bb85
Tomislav Hlevnjak <tomislav.hlevnjak@ecx.io>
2020-11-13 11:05:38 +0100

    generating numbers, add validators, run validators, main adapted.

--- src/main/java/com/example/aep201111/Aep202011Application.java
+++ src/main/java/com/example/aep201111/Aep202011Application.java
@@ -1,13 +1,24 @@
 package com.example.aep201111;
 
+import com.example.evilfizzbuzz.EvilFizzBuzz;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Map;
 
 @SpringBootApplication
 public class Aep202011Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Aep202011Application.class, args);
-	}
 
+		EvilFizzBuzz evilFizzBuzz = new EvilFizzBuzz();
+		Map<Integer, String> numbersMap = evilFizzBuzz.generateNumbers(1, 100);
+//		evilFizzBuzz.addValidator();
+//		evilFizzBuzz.addValidator();
+//		evilFizzBuzz.addValidator();
+//		evilFizzBuzz.addValidator();
+
+		evilFizzBuzz.runValidators(numbersMap);
+	}
 }
--- src/main/java/com/example/evilfizzbuzz/EvilFizzBuzz.java
+++ src/main/java/com/example/evilfizzbuzz/EvilFizzBuzz.java
@@ -1,8 +1,16 @@
 package com.example.evilfizzbuzz;
 
-public class EvilFizzBuzz {
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
-    public EvilFizzBuzz() {
+public class EvilFizzBuzz
+{
+    private List<Validator> validators = new ArrayList<>();
+
+    public EvilFizzBuzz()
+    {
 
         // generate numbers
 
@@ -16,5 +24,29 @@ public class EvilFizzBuzz {
 
         // output
 
+    }
+
+    public Map<Integer, String> generateNumbers(int start, int end)
+    {
+        Map<Integer, String> numbersMap = new LinkedHashMap<>();
+        for (int i = start; i <= end; i++)
+        {
+            numbersMap.put(i, "");
+        }
+
+        return numbersMap;
+    }
+
+    public void addValidator(Validator validator)
+    {
+        validators.add(validator);
+    }
+
+    public void runValidators(Map<Integer, String> numbersMap)
+    {
+        for (Validator validator : validators)
+        {
+            validator.update(numbersMap);
+        }
     }
 }
