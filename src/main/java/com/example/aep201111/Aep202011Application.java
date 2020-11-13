e5fd4fb9f7a11f1f86c55dfa12bcb9aab2de80c7
David Malavasi <david.malavasi@ecx.io>
2020-11-13 12:53:18 +0100

    first user story

--- src/main/java/com/example/aep201111/Aep202011Application.java
+++ src/main/java/com/example/aep201111/Aep202011Application.java
@@ -1,20 +1,29 @@
 package com.example.aep201111;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Aep202011Application {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException
+	{
 		SpringApplication.run(Aep202011Application.class, args);
 
 		System.out.println("seas");
 
 		System.out.println("BasePrice:");
-		String baseprice = System.console().readLine();
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String baseprice = reader.readLine();
 
-		System.out.println("Total Price: 1200");
+		System.out.println("Quantity:");
+		String quantity = reader.readLine();
+
+		int totalNetPrice = Integer.valueOf(baseprice) * Integer.valueOf(quantity);
+		System.out.println("Total Net Price: " + totalNetPrice);
 
 	}
 
